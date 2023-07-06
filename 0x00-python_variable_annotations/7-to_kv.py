a29f7c5a735bfacd784adfc9cc7281be624579e9
root <y.ishimwe2@alustudent.com>
2023-07-06 00:52:20 -0700

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: dtr, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Converts a key and its value to a tuple'''
     return (k, float(v**2))
