ce4084c288d8483f605fd0ce48d5cdf498eb1a39
root <y.ishimwe2@alustudent.com>
2023-07-06 00:35:48 -0700

    Sum-List

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 5'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Computes the sum of a list'''
+    return float(sum(input_list))
