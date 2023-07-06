cfa824819deca73acd6a316e4b336a7b4c421357
root <y.ishimwe2@alustudent.com>
2023-07-06 00:51:33 -0700

    Task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 7'''
+from typing import Union, Tuple
+
+
+def to_kv(k: dtr, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple'''
+    return (k, float(v**2))
